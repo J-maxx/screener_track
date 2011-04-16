1c3079918d0d72c82e587f444dd616d4c7393d4a
Jeff Roth <jeff_roth71@mac.com>
2011-04-16 17:16:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreenerTrack::Application.config.secret_token = '33c52505183f14963512fec8069e128aca4b8fbab0b8d30a798c553b7b353c367b514820d552d398b6eda8559777dcf8811d3847020f7cde88da0f9dbe9c4515'
