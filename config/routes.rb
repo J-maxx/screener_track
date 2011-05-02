0b9715ffa92559138c151ec01103c5a4aca4f80e
Jeff Roth <jeff_roth71@mac.com>
2011-05-02 16:11:03 -0400

    added authentication - signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ScreenerTrack::Application.routes.draw do
 
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  
+  resources :users
+  resources :sessions
 
   match 'about' => 'pages#about'
   match 'faq' => 'pages#faq'
