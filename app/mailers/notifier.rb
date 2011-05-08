8b4da2c11d4c51e0daef2fb83d643b0b5b37e5e7
Charissa Smith <smith.charissa@gmail.com>
2011-05-07 20:33:01 -0400

    changed contact us email to go to screenertrack@gmail.com not mailinator

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
 
     def contact_notification(sender)
       @sender = sender
-      mail(:to => "foobar225@mailinator.com",
+      mail(:to => "screenertrack@gmail.com",
            :from => sender.email,
            :subject => "#{sender.contact_type}")
    end
