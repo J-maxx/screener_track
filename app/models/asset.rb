85dd27fd8175947ae0cc6b70da2808a88625c5af
Jeff Roth <jeff_roth71@mac.com>
2011-05-09 11:49:56 -0400

    Some small changes to asset partial and model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -15,7 +15,7 @@
 
 class Asset < ActiveRecord::Base
   
-  attr_accessible :running_time, :version_name, :media, :cut_description
+  attr_accessible :running_time, :version_name, :media, :cut_description, :project_id
   
   belongs_to :project
   
